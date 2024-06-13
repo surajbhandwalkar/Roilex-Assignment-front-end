db770541be234be9446a681fc7be5db95c84f087
suraj bhandwalkar <suraj.b9930@gmail.com>
2024-06-13 10:48:23 +0530

    front-end

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Transactions from './component/Transactions';
+import './App.css'
+
+const App=()=>{
+  return(
+    <div className='app-container'>
+      <Transactions/>
+    </div>
+  )
+}
+export default App;
---
+++ src/components/Statistics/app.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './style.css'
+
+const Statistics = (props) => {
+  const [statistics, setStatistics] = useState({});
+const {selectedMonth} = props
+
+  useEffect(() => {
+    const getStatistics = async ()=>{
+        const statistics = await axios.get(`'https://s3.amazonaws.com/roxiler.com/product_transaction.json'/?month=${selectedMonth}`)
+        
+            setStatistics(statistics.data)
+
+        
+    }
+    
+
+    getStatistics()
+    
+  }, [selectedMonth]);
+  console.log(statistics)
+
+  return (
+    <div className='statistics-main-container'>
+        <h2>Statistics - {selectedMonth}</h2>
+        <div className='statitics-container'>
+            <div className='element'><span>Total Sale</span> <span>{statistics.totalSaleAmount}</span></div>
+            <div className='element'><span>Total sold item</span> <span>{statistics.totalSoldItems}</span></div>
+            <div className='element'><span>Total not sold item</span> <span>{statistics.totalNotSoldItems}</span></div>
+        </div>
+    </div>
+  );
+};
+
+export default Statistics;
